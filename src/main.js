ccd44c61cdb0e59177f49b6b4e23f6e5488bfb9c
aj <angadjot.b@gmail.com>
2020-11-05 23:57:16 -0500

    Log in button redirects to LinkedIn auth page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+
+import axios from 'axios'
+import VueRouter from 'vue-router'
+
+Vue.use(axios)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter()
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
